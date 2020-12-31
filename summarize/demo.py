8df7c1307805f4971a60a035f42dd304d23467cb
theshiya <shiya993@gmail.com>
2020-12-30 21:05:28 -0500

    added budget

--- summarize/demo.py
+++ summarize/demo.py
@@ -10,10 +10,12 @@ if __name__ == "__main__":
 
     url = "https://www.yelp.com/biz/omars-mediterranean-cuisine-new-york-2"
     n_pages = 2
+    budget = 500
+
     scraper = YelpScraper(url)
     reviews = scraper.scrape(n_pages)
     summarizer = Summarizer(reviews, sim_metric="doc2vec")
-    results = summarizer.summarize(budget=0.1)
+    results = summarizer.summarize(budget=budget)
 
     print("-----------------\nSample output reviews:\n")
     [print("{}: {}\n".format(i + 1, res)) for i, res in enumerate(results[:3])]
